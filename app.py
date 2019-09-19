80937d402961d77b9bd9413006b909d166d7b731
Arturo Rendón <arendon@dgb.unam.mx>
2019-09-19 11:26:12 -0500

    Generación de la base de datos

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
+from models import (Post, Category)
+
+
+@app.cli.command("initdb")
+def init_db():
+    db.create_all()
+
 
 @app.route('/')
 def hello_world():
