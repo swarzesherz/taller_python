39197b128f16ccf8246ba0966e80302913b79720
Arturo Rendón <arendon@dgb.unam.mx>
2019-09-19 11:25:24 -0500

    Generación de ORM para con SQL Alchemy

---
+++ models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from app import db
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False,
+                         default=datetime.utcnow)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
+                            nullable=False)
+    category = db.relationship('Category',
+                               backref=db.backref('posts', lazy=True))
+
+    def __str__(self):
+        return '<Post %r>' % self.title
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __str__(self):
+        return '<Category %r>' % self.name
