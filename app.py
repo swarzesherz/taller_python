c723822f0afe4c729c7c5fe66b744b3bf84be870
Arturo Rendón <arendon@dgb.unam.mx>
2019-09-19 13:27:53 -0500

    Consulta de posts

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import joinedload
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -29,6 +30,16 @@ def create_post():
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/#http-methods
+@app.route('/posts', methods=['GET'])
+def list_posts():
+    query = Category.query.options(joinedload('posts'))
+    result = {}
+    for category in query:
+        result[category.name] = [p.title for p in category.posts]
+    return jsonify(result)
 
 
 if __name__ == '__main__':
