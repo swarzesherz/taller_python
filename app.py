b589ea078ba54d4214343167149f1be45f8221ca
Arturo Rendón <arendon@dgb.unam.mx>
2019-09-19 12:30:15 -0500

    Creación de contenido de prueba desde un comando

--- app.py
+++ app.py
@@ -14,6 +14,18 @@ def init_db():
     db.create_all()
 
 
+@app.cli.command("create-post")
+def create_post():
+    # https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
+    post_category = Category(name='Python')
+    Post(title='Hello Python!', body='Python is pretty cool',
+         category=post_category)
+    post = Post(title='Snakes', body='Ssssssss')
+    post_category.posts.append(post)
+    db.session.add(post_category)
+    db.session.commit()
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
