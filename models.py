c6a3c876f588f45e4b99e7ad4eac8b6c02e983ef
Arturo Rendón <arendon@dgb.unam.mx>
2019-09-20 09:09:03 -0500

    Actualización de modelos a MongoEngine

--- models.py
+++ models.py
@@ -5,55 +5,41 @@ from flask_security import UserMixin, RoleMixin
 from app import db
 
 # Modelos para Usuario y Rol
-# https://pythonhosted.org/Flask-Security/quickstart.html
-roles_users = db.Table(
-    'roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
-)
+# https://pythonhosted.org/Flask-Security/quickstart.html#mongoengine-application
+# http://docs.mongoengine.org/
 
 
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
+class Role(db.Document, RoleMixin):
+    name = db.StringField(max_length=80, unique=True)
+    description = db.StringField(max_length=255)
 
     def __str__(self):
         return self.name
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+class User(db.Document, UserMixin):
+    email = db.StringField(max_length=255)
+    password = db.StringField(max_length=255)
+    active = db.BooleanField(default=True)
+    confirmed_at = db.DateTimeField()
+    roles = db.ListField(db.ReferenceField(Role), default=[])
 
     def __str__(self):
         return self.email
 
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    pub_date = db.Column(db.DateTime, nullable=False,
-                         default=datetime.utcnow)
-
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
-                            nullable=False)
-    category = db.relationship('Category',
-                               backref=db.backref('posts', lazy=True))
-
-    def __str__(self):
-        return '<Post %r>' % self.title
-
-
-class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+class Category(db.Document):
+    name = db.StringField(max_length=50, required=True)
 
     def __str__(self):
         return '<Category %r>' % self.name
+
+
+class Post(db.Document):
+    title = db.StringField(max_length=80, required=True)
+    body = db.StringField()
+    pub_date = db.DateTimeField(default=datetime.utcnow)
+    category = db.ReferenceField(Category)
+
+    def __str__(self):
+        return '<Post %r>' % self.title
