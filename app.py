5352aeee7f2053590d435e98288d554e56e2218f
Arturo Rendón <arendon@dgb.unam.mx>
2019-09-20 09:15:34 -0500

    Uso de admin_views de MongoEngine

--- app.py
+++ app.py
@@ -36,40 +36,15 @@ app.config['FLASK_ADMIN_SWATCH'] = 'flatly'
 admin = Admin(app, name='Admin', template_mode='bootstrap3')
 
 # Admin views
-admin.add_view(CategoryModelView(Category, db.session, name=__('Categorias')))
-admin.add_view(PostModelView(Post, db.session))
-admin.add_view(UserModelView(User, db.session, name=__('Usuarios')))
-admin.add_view(RoleModelView(Role, db.session, name=__('Roles')))
-
-
-@app.cli.command("initdb")
-def init_db():
-    db.create_all()
-
-
-@app.cli.command("create-post")
-def create_post():
-    # https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
-    post_category = Category(name='Python')
-    Post(title='Hello Python!', body='Python is pretty cool',
-         category=post_category)
-    post = Post(title='Snakes', body='Ssssssss')
-    post_category.posts.append(post)
-    db.session.add(post_category)
-    db.session.commit()
+admin.add_view(CategoryModelView(Category, name=__('Categorias')))
+admin.add_view(PostModelView(Post))
+admin.add_view(UserModelView(User, name=__('Usuarios')))
+admin.add_view(RoleModelView(Role, name=__('Roles')))
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
-
-# https://flask.palletsprojects.com/en/1.1.x/quickstart/#http-methods
-@app.route('/posts', methods=['GET'])
-@login_required
-def list_posts():
-    categories = Category.query.options(joinedload('posts')).all()
-    return render_template('posts.html', categories=categories)
 
 
 if __name__ == '__main__':
